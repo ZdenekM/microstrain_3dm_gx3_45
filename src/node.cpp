9fe4d0e3f3035048f533030ced67f983e69b1a85
ZdenekM <imaterna@fit.vutbr.cz>
2013-07-15 09:19:22 +0200

    Change default value of IMU frame_id.

--- src/node.cpp
+++ src/node.cpp
@@ -18,7 +18,7 @@ imuNode::imuNode() : nh_priv_("~") {
 	param::param<string>("~port",port_,"/dev/ttyACM0");
 	param::param<int>("~baud_rate",baud_rate_,115200);
 	param::param<float>("~declination",declination_, 3.8); // http://www.ngdc.noaa.gov/geomag-web/#declination
-	param::param<string>("~frame_id",frame_id_,"/imu");
+	param::param<string>("~frame_id",frame_id_,"/imu_link");
 	param::param<string>("~child_frame_id",child_frame_id_,"/base_footprint");
 	param::param<float>("~rate",rate_,10.0);
 
